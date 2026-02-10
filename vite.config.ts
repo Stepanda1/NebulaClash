1151fde072fa4d8401446da6eb29e81db09e0237
Stepan <stalkozlov@edu.hse.ru>
2026-02-11 01:20:35 +0500

    Fix Vite base for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 export default defineConfig({
-  base: '/match3-game/',
+  base: isGitHubPages ? '/match3-game/' : '/',
   plugins: [react()],
 })
