fc7c538ac09f39fb8323c139754ec13be38d5e9b
Stepan <stalkozlov@edu.hse.ru>
2026-02-25 23:22:51 +0500

    Remove GitHub Pages base path config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
-
 export default defineConfig({
-  base: isGitHubPages ? '/match3-game/' : '/',
+  base: '/',
   plugins: [react()],
   server: {
     proxy: {
