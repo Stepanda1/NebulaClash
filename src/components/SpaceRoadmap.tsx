0d085c495082c47efd8df99f129235374bd80b36
Stepan <stalkozlov@edu.hse.ru>
2026-03-08 21:09:28 +0500

    Fix roadmap scroll container height on mobile

--- src/components/SpaceRoadmap.tsx
+++ src/components/SpaceRoadmap.tsx
@@ -232,7 +232,7 @@ export function SpaceRoadmap({
           </div>
         </div>
 
-        <div className="relative flex-1">
+        <div className="relative flex-1 min-h-0">
           <div className="absolute left-2 top-2 z-30 flex flex-col gap-2">
             <button
               type="button"
